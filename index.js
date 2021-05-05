720b7968508a26249a7a6a50364f773faeb7970a
ricardo <r.f.c.m.29@gmail.com>
2021-05-05 11:33:40 +0100

    instalacao de dependencias

---
+++ index.js
@@ -0,0 +1,9 @@
+// Create a new Express application
+var app = express();
+
+// Create an http server with Node's HTTP module. 
+// Pass it the Express application, and listen on port 8080. 
+var server = require('http').createServer(app).listen(8080);
+
+// Instantiate Socket.IO hand have it listen on the Express/HTTP server
+var io = require('socket.io').listen(server);
